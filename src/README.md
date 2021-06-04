c5d624f1694ef1dddc967cefba62f147c6e3e116
Omarei <omareigregory@gmail.com>
2021-06-04 15:16:38 -0600

    first commit

---
+++ src/README.md
@@ -0,0 +1,13 @@
+## BlackJack Project
+
+### Overview
+
+### Technologies Used
+* Java
+* Enums
+* Object Oriented design
+* Git
+*
+*
+
+## Lessons Learned
---
+++ src/com/skilldistillery/cards/blackjack/BlackJackApp.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.cards.blackjack;
+
+public class BlackJackApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
